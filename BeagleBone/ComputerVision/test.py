f92f0dc1fa5d4f91b0d3f9cf69573d50b6a1f4c8
Ryan <ryan.joyce@wsu.edu>
2016-06-24 18:29:08 -0700

    Update 1
    Updated the script and added the template images

--- BeagleBone/ComputerVision/test.py
+++ BeagleBone/ComputerVision/test.py
@@ -1,23 +1,51 @@
-from SimpleCV import Camera
+#!/usr/bin/ python
+import SimpleCV
 
 # Initialize the camera
 cam = Camera()
 
-# Loop to continuously get images
+# Initialize the template image
+template = Image("template.bmp")
+
+# Extract the yellow
+temp_yellow = template.colorDistance(Color.YELLOW)
+temp_yellow.save("temp_yellow.png") # TODO: Remove
+only_yellow = template - temp_yellow
+only_yellow.save("only_yellow.png") # TODO: Remove
+
+# Get the mean colors of the template image
+template_mean_colors = only_yellow.meanColor()
+
+# Get image from the camera
+img = cam.getImage()
+
+# Loop to continuously get images from the camera
 while True:
-    # Get Image from camera
+    # Get image from camera
     img = cam.getImage()
-    # Make image black and white
-    #img = img.binarize() // Shits broke yo
 
-    # Process the image
-    # use a keypoint detector to find areas of interest
-    feats = img.findKeypoints()
-    # draw the list of keypoints
-    feats.draw(color=Color.RED)
-    # apply the stuff we found to the image.
-    output = img.applyLayers()
+    # Extract the yellow
+    img_yellow = img.colorDistance(Color.YELLOW)
+    img_only_yellow = img - img_yellow
 
-    # Save the image(s)
-    img.save("img.png")
-    output.save("img_keypoints.png")
+    # Get the mean colors of the current image
+    img_mean_colors = img_only_yellow.meanColor()
+
+    # Compare the images
+    # pseudo:
+    # if R > ??? and G > ??? and B > ???:
+    #    we can see the F!
+    #    break and start the landing script
+
+# Save the image(s)
+img.save("img.png")
+
+
+# - keypoint Stuff -
+# Process the image
+# use a keypoint detector to find areas of interest
+#feats = img.findKeypoints()
+# draw the list of keypoints
+#feats.draw(color=Color.RED)
+# apply the stuff we found to the image.
+#output = img.applyLayers()
